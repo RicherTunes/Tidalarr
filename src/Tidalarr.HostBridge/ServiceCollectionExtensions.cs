d903cbd67abb79504d9588bcbb9a714c4486535c
RicherTunes <xfear26@hotmail.com>
2025-10-11 11:37:22 -0400

    feat(hostbridge): DI extension AddTidalarrHostBridgeServices to register IHostSettingsMapper

---
+++ src/Tidalarr.HostBridge/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tidalarr.HostBridge;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTidalarrHostBridgeServices(this IServiceCollection services)
+    {
+        services.AddSingleton<Settings.IHostSettingsMapper, Settings.HostSettingsMapper>();
+        return services;
+    }
+}
+
